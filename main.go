90b39ee5baaba8b48bc9869c89ae070ebccbb56b
AMF <work@afdesk.com>
2021-07-21 12:44:52 +0600

    Add showing logs into a file

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"gopkg.in/yaml.v2"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 )
@@ -23,10 +25,20 @@ func init() {
 
 const currentVersion = "v0.1.8"
 
+func setMainLogFile(dir string) error {
+	fn := fmt.Sprintf("%s/sys_%s.txt", dir, app.GetCurrentDateName())
+	fMainLogs, err := os.OpenFile(fn, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0x666)
+	if err != nil {
+		return err
+	}
+	log.SetOutput(io.MultiWriter(os.Stdout, fMainLogs))
+	log.Printf("File for main logs: %q", fn)
+	return nil
+}
+
 func main() {
 	rand.Seed(12212112)
 	flag.Parse()
-	log.Printf("Running ETP AutoCopy %s", currentVersion)
 	b, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		log.Fatal(err)
@@ -36,23 +48,27 @@ func main() {
 		log.Fatal(err)
 	}
 	cfg.LogConfig()
+
 	runner(cfg)
 }
 
 func runner(cfg *app.Config) {
-	tick := time.NewTicker(time.Millisecond * time.Duration(cfg.PollInterval))
-
-	const checkMoreOften = 100
-	checkMounter := time.NewTicker(time.Millisecond * time.Duration(cfg.PollInterval) * checkMoreOften)
-	log.Print("Start listening of new USB flashes")
-
 	if err := app.MountRemoteServer(cfg.UploadPath, cfg.LocalEndpoint); err != nil {
 		log.Fatalf("MountRemoteServer fatal error: %v", err)
 	}
+	folder := fmt.Sprintf("%s/%s", cfg.LocalEndpoint, strings.TrimPrefix(cfg.Folder, "/"))
 
-	f := strings.TrimPrefix(cfg.Folder, "/")
-	folder := fmt.Sprintf("%s/%s", cfg.LocalEndpoint, f)
+	if err := setMainLogFile(folder); err != nil {
+		log.Fatalf("setMainLogFile error: %v", err)
+	}
 
+	log.Printf("Running ETP AutoCopy %s", currentVersion)
+
+	tick := time.NewTicker(time.Millisecond * time.Duration(cfg.PollInterval))
+	const checkMoreOften = 100
+	checkMounter := time.NewTicker(time.Millisecond * time.Duration(cfg.PollInterval) * checkMoreOften)
+
+	log.Print("Start listening of new USB flashes")
 	for {
 		select {
 		case <-tick.C:
